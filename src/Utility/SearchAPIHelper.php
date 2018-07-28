5fbdc746eb1589538dd38f5f55b920bbd07dddb1
kyuubi <duarte.garin@gmail.com>
2018-07-28 19:19:41 +1000

    Adding Utility class.
    Utility class to have some methods like the check of multivalue fields
    which in fact doesn't work (It didn't in the previous version either. I
    haven't fixed it yet but will address that in a later commit)

---
+++ src/Utility/SearchAPIHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\graphql_search_api\Utility;
+
+/**
+ * TODO
+ */
+class SearchAPIHelper {
+
+  /**
+   * Checks storage if a field is multivalued.
+   *
+   * @param $field
+   */
+  public static function checkMultivalue($field) {
+
+    $multivalue = FALSE;
+
+    if (!empty($field->getDatasourceId())) {
+
+      // Obtain details needed to load the field from storage.
+      $datasource_id = explode(':', $field->getDatasourceId())[1];
+      $property_path = $field->getPropertyPath();
+      $field_name_original = explode(':', $property_path)[0];
+
+      // Load the field from storage.
+      $field_storage_config = \Drupal::entityTypeManager()
+        ->getStorage('field_storage_config')
+        ->load($datasource_id . '.' . $field_name_original);
+
+      // Set field as multivalue if it has unlimited cardinality.
+      if (isset($field_storage_config) && $field_storage_config->getCardinality() == FieldStorageConfigInterface::CARDINALITY_UNLIMITED) {
+        $multivalue = TRUE;
+      }
+    }
+
+    return $multivalue;
+  }
+}
+
