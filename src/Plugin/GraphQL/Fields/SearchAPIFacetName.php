0fadfb05ba959888548cc40d309b2c1f928262ba
kyuubi <duarte.garin@gmail.com>
2018-07-28 19:39:28 +1000

    Renaming the Facet Name type to Search API for consistency.
    Also changed the 'name' to 'name' since its scoped safely inside the
    Facet type.

--- src/Plugin/GraphQL/Fields/SearchAPIFacetName.php
+++ src/Plugin/GraphQL/Fields/SearchAPIFacetName.php
@@ -11,18 +11,18 @@ use GraphQL\Type\Definition\ResolveInfo;
  *
  * @GraphQLField(
  *   secure = true,
- *   parents = {"SolrFacet"},
- *   id = "solr_facet_name",
- *   name = "solrFacetName",
+ *   parents = {"SearchAPIFacet"},
+ *   id = "search_api_facet_name",
+ *   name = "name",
  *   type = "String"
  * )
  */
-class SolrFacetName extends FieldPluginBase {
+class SearchAPIFacetName extends FieldPluginBase {
 
   /**
    * {@inheritdoc}
    */
   public function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
-    yield $value['solrFacetName'];
+    yield $value['name'];
   }
 }
