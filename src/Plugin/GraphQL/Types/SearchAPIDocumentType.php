a355d132067aeb5538a57444015ab282f3533159
kyuubi <duarte.garin@gmail.com>
2018-07-28 19:20:53 +1000

    Adding Search API Document Type class.
    A document type that represents a Search API document. This leverages
    the Document Type deriver to generate different types for each index.

---
+++ src/Plugin/GraphQL/Types/SearchAPIDocumentType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\graphql_search_api\Plugin\GraphQL\Types;
+
+use Drupal\graphql\GraphQL\Execution\ResolveContext;
+use Drupal\graphql\Plugin\GraphQL\Types\TypePluginBase;
+use GraphQL\Type\Definition\ResolveInfo;
+
+/**
+ * A Search API document type.
+ *
+ * @GraphQLType(
+ *   id = "search_api_document_type",
+ *   interfaces = {"SearchAPIDocument"},
+ *   deriver = "Drupal\graphql_search_api\Plugin\GraphQL\Derivers\SearchAPIDocumentTypeDeriver"
+ * )
+ */
+class SearchAPIDocumentType extends TypePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies($object, ResolveContext $context, ResolveInfo $info) {
+
+    // Retrieve the document type derivative.
+    $derivative_id = $this->getDerivativeId();
+
+    return $object['type'] == $derivative_id;
+  }
+}
